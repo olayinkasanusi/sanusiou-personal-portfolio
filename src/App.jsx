8457e5e804f5942aa69b63eabb0dc11f0e963949
Sanusi Olayinka <222006189+olayinkasanusi@users.noreply.github.com>
2025-11-22 15:23:56 +0100

    Fixed a bug in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Spinner from "./ui/Spinner";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ScrollToTop from "./ui/ScrollToTop";
+import { HelmetProvider } from "react-helmet-async";
 
 const HeroSection = lazy(() => import("./pages/HeroSection"));
 const ContactMe = lazy(() => import("./pages/ContactMe"));
@@ -25,44 +26,49 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        {/* <ReactQueryDevtools /> */}
-        <BrowserRouter>
-          <ScrollToTop />
-          <Suspense fallback={<Spinner />}>
-            <Routes>
-              <Route path="/" index element={<HeroSection />} />
-              <Route path="aboutme" element={<AboutMe />} />
-              <Route path="projects" element={<Projects />} />
-              <Route path="contactme" element={<ContactMe />} />
-              <Route path="projects/:projectId" element={<ProjectDetails />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
-        <Toaster
-          position="top-center"
-          gutter={12}
-          containerStyle={{ margin: "8px" }}
-          toastOptions={{
-            success: {
-              duration: 5000,
-            },
-            error: {
-              duration: 5000,
-            },
-            style: {
-              fontFamily: "sans-serif",
-              fontSize: "16px",
-              maxWidth: "500px",
-              padding: "16px 24px",
-              backgroundColor: "white",
-              color: "var(--color-grey-700)",
-            },
-          }}
-        />
-      </QueryClientProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          {/* <ReactQueryDevtools /> */}
+          <BrowserRouter>
+            <ScrollToTop />
+            <Suspense fallback={<Spinner />}>
+              <Routes>
+                <Route path="/" index element={<HeroSection />} />
+                <Route path="aboutme" element={<AboutMe />} />
+                <Route path="projects" element={<Projects />} />
+                <Route path="contactme" element={<ContactMe />} />
+                <Route
+                  path="projects/:projectId"
+                  element={<ProjectDetails />}
+                />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </Suspense>
+          </BrowserRouter>
+          <Toaster
+            position="top-center"
+            gutter={12}
+            containerStyle={{ margin: "8px" }}
+            toastOptions={{
+              success: {
+                duration: 5000,
+              },
+              error: {
+                duration: 5000,
+              },
+              style: {
+                fontFamily: "sans-serif",
+                fontSize: "16px",
+                maxWidth: "500px",
+                padding: "16px 24px",
+                backgroundColor: "white",
+                color: "var(--color-grey-700)",
+              },
+            }}
+          />
+        </QueryClientProvider>
+      </HelmetProvider>
     </>
   );
 }
